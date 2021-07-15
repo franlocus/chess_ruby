5e31f0f0558367519e7c9796b025adae6c0c0a59
Fran <78037877+franlocus@users.noreply.github.com>
2021-07-15 19:19:05 -0300

    king

--- lib/king.rb
+++ lib/king.rb
@@ -1,10 +1,13 @@
 class King < Piece
   
-  def legal_moves
+  def legal_moves(board)
     moves_vector = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
     moves_vector
       .map { |y, x| [square[0] + y, square[1] + x] }
-      .keep_if { |move| move[0].between?(0, 7) && move[1].between?(0, 7) }
+      .keep_if do |move|
+        move[0].between?(0, 7) && move[1].between?(0, 7) &&
+          (!board.piece?(move) || board.color_piece(move) != color)
+      end
   end
   
   def unicode
