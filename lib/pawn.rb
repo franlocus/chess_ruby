5eabd5a806a422e369b88ddf6c0d94ea59f0cbae
Fran <78037877+franlocus@users.noreply.github.com>
2021-07-27 12:29:41 -0300

    fixed bug pawn capturing with  2 moves

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -10,35 +10,38 @@ class Pawn < Piece
       down_moves(board) + down_diagonals_attack(board, defence_move) + en_passant_move(board, board.player_white, 6, 4, 5)
     end
   end
-  # peon si es blanco en row 3 [3, n], si es negro [4, n]
-  # el ultimo movimiento del enemigo es un peon a la misma fila [3, n+1 || n-1]
 
   def en_passant_move(board, player, from_row, to_row, capture_row)
-    return [] unless en_passant?(board, player, from_row, to_row)
-
-    @en_passant = [[capture_row, player.last_turn_to[1]]]
-  end
-
-  def en_passant?(board, player, from_row, to_row)
-    return false unless @square.first.eql?(to_row)
-
     last_turn_from = player.last_turn_from
     last_turn_to = player.last_turn_to
-    board.fetch_piece(player.last_turn_to).is_a?(Pawn) &&
-    (last_turn_from.first.eql?(from_row) && last_turn_to.first.eql?(to_row)) &&
-    last_turn_to[1].between?(square[1] - 1, square[1] + 1)
+    return [] if last_turn_to.nil?
+
+    last_piece = board.fetch_piece(last_turn_to)
+    return [] unless en_passant?(last_piece, from_row, to_row, last_turn_from[0], last_turn_to)
+
+    @en_passant = [[capture_row, last_turn_to[1]]]
+  end
+
+  def en_passant?(last_piece, from_row, to_row, last_turn_from, last_turn_to)
+    return false unless @square.first == to_row
+
+    last_piece.is_a?(Pawn) &&
+      ((last_turn_from == from_row) && (last_turn_to[0] == to_row)) &&
+      last_turn_to[1].between?(square[1] - 1, square[1] + 1)
   end
 
   def up_moves(board)
     return [] if board.piece?([square[0] - 1, square[1]])
+    return upward_moves(board).first(2) unless @has_moved || board.piece?([square[0] - 2, square[1]])
 
-    @has_moved == false ? upward_moves(board).first(2) : [upward_moves(board).first]
+    [upward_moves(board).first]
   end
 
   def down_moves(board)
     return [] if board.piece?([square[0] + 1, square[1]])
+    return downward_moves(board).first(2) unless @has_moved || board.piece?([square[0] + 2, square[1]])
 
-    @has_moved == false ? downward_moves(board).first(2) : [downward_moves(board).first]
+    [downward_moves(board).first]
   end
 
   def up_diagonals_attack(board, defence_move)
@@ -52,6 +55,6 @@ class Pawn < Piece
   end
 
   def unicode
-    @color == "white" ? "♙ ".magenta : "♟︎ ".yellow
+    @color == 'white' ? '♙ '.magenta : '♟︎ '.yellow
   end
 end
