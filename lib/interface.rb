1792ea9508570195719d0ddefa170aed4d17472e
Fran <78037877+franlocus@users.noreply.github.com>
2021-08-02 20:30:06 -0300

    prompt user and validate

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 class Interface
-
   def welcome
     puts "Welcome to Chess!\nGet ready to play, enjoy."
+  end
+
+  def prompt_valid_input
+    puts "\nPlayer #color please enter the piece you would like to move:"
+    validate(gets.chomp) || try_again
+  end
+
+  def validate(input)
+    return input if input.match?(/^[a-h][1-8]$/)
+  end
+
+  def try_again
+    puts "Sorry, input error! Please introduce a valid algebraic notation, eg. 'a1' or 'b5'".red
+    prompt_valid_input
+  end
+  # TODO Complete who will receive the output from prompt to coordinates
+  def to_coordinates(algebraic)
+    algebraic = algebraic.chars
+    [(algebraic.last.to_i - 8).abs, algebraic.first.ord - 97]
   end
 end
--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/interface'
+require_relative '../lib/colorize'
 
 describe Interface do
   describe '#welcome' do
@@ -9,5 +10,32 @@ describe Interface do
       subject.welcome
     end
   end
-  
+  describe '#prompt_valid input' do
+    before do
+      allow(subject).to receive(:puts)
+    end
+    context 'user input a valid answer and validate correctly' do
+      before do
+        valid_input = 'a1'
+        allow(subject).to receive(:gets).and_return(valid_input)
+      end
+      it 'returns the input when is a valid algebraic format' do
+        expect(subject).to receive(:gets).and_return('a1')
+        subject.prompt_valid_input
+      end
+    end
+    context 'user input invalid first and triggers try_again, then a valid' do
+      before do
+        invalid_input = 'ab1'
+        valid_input = 'a1'
+        allow(subject).to receive(:gets).and_return(invalid_input, valid_input)
+      end
+      it 'trigger try_again once when is an invalid algebraic format' do
+        expect(subject).to receive(:gets).twice
+        expect(subject).to receive(:puts).exactly(3) # Intructions, error, intructions
+        subject.prompt_valid_input
+      end
+    end
+    
+  end
 end
