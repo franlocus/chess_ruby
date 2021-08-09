e6dfde343fbd67069421c10c7f2532d4f46f72e2
Fran <78037877+franlocus@users.noreply.github.com>
2021-08-09 12:51:44 -0300

    king legal moves

--- lib/moves_calculator.rb
+++ lib/moves_calculator.rb
@@ -7,11 +7,28 @@ class MovesCalculator
     @board = board
   end
 
-  def legal_moves(coordinates)
+  def legal_moves(coordinates, is_white)
     piece = board.piece(coordinates)
-    piece.moves(board)
+    if piece.is_a?(King)
+      piece.moves(board, player_attacked_squares(!is_white))
+    else
+      piece.moves(board)
+    end
   end
 
+  def player_attacked_squares(is_white)
+    attacked = []
+    board.squares.each do |row|
+      row.each do |square|
+        next if square.nil? || square.is_white != is_white
+
+        attacked += square.is_a?(King) ? square.around_squares : square.moves(board, true)
+      end
+    end
+    attacked
+  end
+
+# TODO: UNDER pin? 
   def legal_moves2(coordinates, player, checker = nil)
     piece = fetch_piece(coordinates)
     if under_pin?(piece, player.king, checker)
