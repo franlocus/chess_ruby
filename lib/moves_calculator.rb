4441ba2b0b3be918a228ca930d8ad6947a0e46fc
Fran <78037877+franlocus@users.noreply.github.com>
2021-08-10 13:54:27 -0300

    refactored under pin, pinned_fireline

--- lib/moves_calculator.rb
+++ lib/moves_calculator.rb
@@ -7,22 +7,40 @@ class MovesCalculator
     @board = board
   end
 
-  def legal_moves(coordinates, is_white)
+  def legal_moves(coordinates, is_white) # checker = false
     piece = board.piece(coordinates)
     if piece.is_a?(King)
       piece.moves(board, enemy_defended_squares(is_white))
-    else
       # need to cover the case when is check and need to check under_pin? double check situation 
       # require to determine first the real checker so not collide with simulated checker 
-      if under_pin?(piece, is_white)
-        return [] if piece.is_a?(Knight)
-        king = board.king(is_white)
-        # checker = false at the moment, complete when checking state for force move
-        pinned_fireline(piece, king, false) & piece.moves(board)
-      else
-        piece.moves(board)
-      end
+    elsif under_pin?(piece, is_white)
+      return [] if piece.is_a?(Knight)
+
+      king = board.king(is_white)
+      # checker = false at the moment, complete when checking state for force move
+      piece.moves(board) & moves_to_pinner_and_king(piece, king, false)
+    else
+      piece.moves(board)
     end
+  end
+
+  def moves_to_pinner_and_king(piece, king, checker)
+    toward_pinner(piece, king, checker) + toward_king(piece, king)
+  end
+
+  def toward_pinner(piece, king, checker)
+    moves = []
+    pinner = fetch_pinner(piece, king, checker)
+    pinner.moves(board, false, 'Hash').each_value { |value| moves += value if value.include?(piece.square) }
+    moves << pinner.square
+  end
+
+  def toward_king(piece, king)
+    moves = []
+    return moves if piece.is_a?(Pawn)
+
+    piece.moves(board, true, 'Hash').each_value { |value| moves += value if value.include?(king.square) }
+    moves
   end
 
   def fetch_pinner(piece, king, checker)
@@ -40,35 +58,6 @@ class MovesCalculator
     end
     # returns nil in case square == checker, situation when there is double check
     nil
-  end
-
-  def pinned_fireline(piece, king, checker)
-    #pinner = fetch_pinner(piece, king, checker)
-   
-    #pinner.moves(board, false, 'Hash').each_value { |value| moves += value if value.include?(piece.square) }
-    #unless piece.is_a?(Pawn)
-    #  piece.moves(board, true, 'Hash').each_value { |value| moves += value if value.include?(king.square) }
-    #  moves.delete(king.square)
-    #end
-    #moves.delete(piece.square)
-    #moves << pinner.square
-    
-    toward_pinner(piece, king, checker) + toward_king(piece, king)
-  end
-
-  def toward_pinner(piece, king, checker)
-    moves = []
-    pinner = fetch_pinner(piece, king, checker)
-    pinner.moves(board, false, 'Hash').each_value { |value| moves += value if value.include?(piece.square) }
-    moves << pinner.square
-  end
-
-  def toward_king(piece, king)
-    moves = []
-    return moves if piece.is_a?(Pawn)
-
-    piece.moves(board, true, 'Hash').each_value { |value| moves += value if value.include?(king.square) }
-    moves
   end
 
   def under_check?(is_white, normal_or_simulated_board = board)
@@ -94,23 +83,9 @@ class MovesCalculator
     under_check?(is_white, board_clone)
   end
 
- 
-
   def simulate_a_board_without_piece(piece_square)
     board_clone = Marshal.load(Marshal.dump(board)) # deep copy trick
     board_clone.delete_piece(piece_square)
     board_clone
-  end
-
-# TODO: UNDER pin? 
-  def legal_moves2(coordinates, player, checker = nil)
-    piece = fetch_piece(coordinates)
-    if under_pin?(piece, player.king, checker)
-      return [] if piece.is_a?(Knight)
-
-      pinned_fireline(piece, player.king, checker) & piece.legal_moves(@board)
-    else
-      piece.legal_moves(@board)
-    end
   end
 end
